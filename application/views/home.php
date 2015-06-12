aaf6f88a14606eca7daf6162ca2eeea3166a1a4d
Leonardo Montilla <leonardomo84@gmail.com>
2015-06-12 22:16:06 +0200

    probando para actualizar todo

--- application/views/home.php
+++
@@ -1,8 +0,0 @@
-<ul class='clearing-thumbs small-block-grid-4'>
-<?php foreach ($imagenes as $imagen): ?>
-  <li class='prev'>
-    <?= img($imagen['thumb_url']) ?>
-    <p>por <?= $imagen['nick'] ?></p>
-  </li>
-<?php endforeach ?>
-</ul>
